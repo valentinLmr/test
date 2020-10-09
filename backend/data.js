e3dd1b7749c0ec8ce7f6b3290a54e6579206ea8f
valentin lemaire <valentinlmr28@gmail.com>
2020-10-09 17:26:42 +0200

    back and front and reorganise

---
+++ backend/data.js
@@ -0,0 +1,319 @@
+export default {
+  products: [
+    {
+      id: 1,
+      image: "../image/OIP.jpeg",
+      name: "tunique bandilas",
+      categories: "Robe",
+      brand: "Vans",
+      price: 40,
+      colors: [
+        {
+          color: "red",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 0,
+            },
+          ],
+        },
+        {
+          color: "blue",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 0,
+            },
+          ],
+        },
+        {
+          color: "green",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 0,
+            },
+          ],
+        },
+      ],
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      id: 2,
+      image: "../image/OIP.jpeg",
+      name: "tunique banditas",
+      categories: "Robe",
+      brand: "Veja",
+      price: 40,
+      colors: [
+        {
+          color: "red",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+        {
+          color: "blue",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+        {
+          color: "green",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+      ],
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      id: 3,
+      image: "../image/OIP.jpeg",
+      name: "tunique bandilas",
+      categories: "dress",
+      brand: "Veja",
+      price: 40,
+      colors: [
+        {
+          color: "red",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+        {
+          color: "blue",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+        {
+          color: "green",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+      ],
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      id: 4,
+      image: "../image/OIP.jpeg",
+      name: "tunique bandilas",
+      categories: "dress",
+      brand: "Banditas",
+      price: 40,
+      colors: [
+        {
+          color: "red",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+        {
+          color: "blue",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+        {
+          color: "green",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+      ],
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      id: 5,
+      image: "../image/OIP.jpeg",
+      name: "tunique bandilas",
+      categories: "dress",
+      brand: "Banditas",
+      price: 40,
+      colors: [
+        {
+          color: "red",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+        {
+          color: "blue",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+        {
+          color: "green",
+          sizes: [
+            {
+              size: "s",
+              quantity: 2,
+            },
+            {
+              size: "m",
+              quantity: 2,
+            },
+            {
+              size: "xl",
+              quantity: 2,
+            },
+          ],
+        },
+      ],
+      rating: 4.5,
+      numReviews: 10,
+    },
+  ],
+};
